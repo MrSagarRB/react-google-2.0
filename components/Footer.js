61ea818956176ebdb5f03b81d5c7dbf363281553
Sagar Borude <sagar.html5@gmail.com>
2022-01-08 23:03:12 +0530

    Made body section

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import {GlobeIcon} from "@heroicons/react/solid"
+
+
+
+
+function Footer() {
+    return (
+        <footer>
+     <div>
+         <p>United Kingdom</p>
+     </div>
+
+
+            
+        </footer>
+    )
+}
+
+export default Footer
